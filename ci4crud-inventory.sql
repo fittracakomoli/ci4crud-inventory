8398c944225deb2f3b5593c59a933d51423cef5a
Fittra Marga Ardana <fittracakomoli6@gmail.com>
2026-01-07 09:47:24 +0700

    test: add database for testing

--- ci4crud-inventory.sql
+++ ci4crud-inventory.sql
@@ -39,9 +39,24 @@ CREATE TABLE IF NOT EXISTS `barang` (
 INSERT INTO `barang` (`id`, `id_kategori`, `nama_barang`, `deskripsi`, `stok`, `harga`, `gambar`, `created_at`, `updated_at`) VALUES
 	(8, 1, 'Noir M1 Nex', 'Mouse Gaming Wireless', 41, 270000, '1767521446_728a23e6d1c633751cfb.webp', '2026-01-04 17:10:46', '2026-01-05 15:27:57'),
 	(9, 1, 'Redragon K630W RGB', 'Mechanical Keyboard', 70, 350000, '1767521834_43a3d1292f8ea3a933bb.webp', '2026-01-04 17:17:14', '2026-01-05 15:30:24'),
-	(10, 4, 'Xiaomi G24i 2026', 'Monitor Gaming 200Hz', 9, 1650000, '1767527552_e6b0f58224d81e0c9110.jpg', '2026-01-04 18:52:32', '2026-01-05 15:31:22'),
+	(10, 4, 'Xiaomi G24i 2026', 'Monitor Gaming 200Hz', 38, 1650000, '1767527552_e6b0f58224d81e0c9110.jpg', '2026-01-04 18:52:32', '2026-01-06 16:56:51'),
 	(11, 1, 'Rexus Asta GX150', 'Gamepad Gaming Rexus', 10, 280000, '1767527925_95a624df6d4e8653a8a3.jpg', '2026-01-04 18:58:45', '2026-01-05 16:56:27'),
-	(14, 7, 'Asus Vivobook 1E1404FA', 'FHD321', 15, 6500000, '1767622664_f2bd5fb08a16960767f8.webp', '2026-01-05 21:17:44', '2026-01-05 21:20:43');
+	(14, 7, 'Asus Vivobook 1E1404FA', 'FHD321', 18, 6500000, '1767622664_f2bd5fb08a16960767f8.webp', '2026-01-05 21:17:44', '2026-01-06 16:59:37');
+
+-- Dumping structure for table ci4crud-inventory.divisi
+CREATE TABLE IF NOT EXISTS `divisi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_divisi` varchar(255) NOT NULL,
+  `pj` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table ci4crud-inventory.divisi: ~2 rows (approximately)
+INSERT INTO `divisi` (`id`, `nama_divisi`, `pj`, `created_at`, `updated_at`) VALUES
+	(1, 'Teknologi', 'Mas David', NULL, '2026-01-06 15:29:41'),
+	(3, 'HR', 'Mas Kamal', '2026-01-06 15:23:19', '2026-01-06 15:23:19');
 
 -- Dumping structure for table ci4crud-inventory.kategori
 CREATE TABLE IF NOT EXISTS `kategori` (
@@ -60,10 +75,29 @@ INSERT INTO `kategori` (`id`, `nama`, `keterangan`, `created_at`, `updated_at`) 
 	(4, 'PC', 'Personal Computer', '2026-01-05 10:19:27', '2026-01-05 10:19:27'),
 	(7, 'Laptop', 'Semua jenis laptop', '2026-01-05 21:11:30', '2026-01-05 21:11:30');
 
+-- Dumping structure for table ci4crud-inventory.supplier
+CREATE TABLE IF NOT EXISTS `supplier` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nama_supplier` varchar(255) NOT NULL,
+  `kontak` varchar(255) NOT NULL,
+  `alamat` text,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table ci4crud-inventory.supplier: ~2 rows (approximately)
+INSERT INTO `supplier` (`id`, `nama_supplier`, `kontak`, `alamat`, `created_at`, `updated_at`) VALUES
+	(1, 'Xiaomi Indonesia', 'contact@xiaomi.com', 'Jakarta Pusat', NULL, NULL),
+	(3, 'Asus Indonesia', 'contact@asus.co.id', 'Jakarta Barat', '2026-01-06 16:17:32', '2026-01-06 16:17:43');
+
 -- Dumping structure for table ci4crud-inventory.transaksi_stok
 CREATE TABLE IF NOT EXISTS `transaksi_stok` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_barang` int NOT NULL,
+  `id_supplier` int DEFAULT NULL,
+  `id_divisi` int DEFAULT NULL,
+  `invoice` text NOT NULL,
   `jenis` enum('masuk','keluar') NOT NULL,
   `jumlah` decimal(65,0) NOT NULL DEFAULT '0',
   `keterangan` text,
@@ -71,16 +105,17 @@ CREATE TABLE IF NOT EXISTS `transaksi_stok` (
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_barang` (`id_barang`),
-  CONSTRAINT `fk_barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_supplier` (`id_supplier`),
+  KEY `fk_divisi` (`id_divisi`),
+  CONSTRAINT `fk_barang` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_divisi` FOREIGN KEY (`id_divisi`) REFERENCES `divisi` (`id`),
+  CONSTRAINT `fk_supplier` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table ci4crud-inventory.transaksi_stok: ~5 rows (approximately)
-INSERT INTO `transaksi_stok` (`id`, `id_barang`, `jenis`, `jumlah`, `keterangan`, `created_at`, `updated_at`) VALUES
-	(2, 8, 'keluar', 5, 'Perawatan', '2026-01-05 15:27:57', '2026-01-05 15:27:57'),
-	(3, 9, 'keluar', 5, 'Perawatan', '2026-01-05 15:30:24', '2026-01-05 15:30:24'),
-	(4, 10, 'masuk', 5, 'Stok baru', '2026-01-05 15:31:22', '2026-01-05 15:31:22'),
-	(5, 11, 'keluar', 8, 'Pembelian', '2026-01-05 16:56:27', '2026-01-05 16:56:27'),
-	(13, 14, 'masuk', 15, 'Tambah stok', '2026-01-05 21:20:43', '2026-01-05 21:20:43');
+-- Dumping data for table ci4crud-inventory.transaksi_stok: ~2 rows (approximately)
+INSERT INTO `transaksi_stok` (`id`, `id_barang`, `id_supplier`, `id_divisi`, `invoice`, `jenis`, `jumlah`, `keterangan`, `created_at`, `updated_at`) VALUES
+	(18, 10, 1, NULL, '20260106165651/4D730B', 'masuk', 4, 'Stok baru', '2026-01-06 16:56:51', '2026-01-06 16:56:51'),
+	(19, 14, NULL, 1, '165937D93560', 'keluar', 4, 'Kebutuhan', '2026-01-06 16:59:37', '2026-01-06 16:59:37');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
